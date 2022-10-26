6c18d403afda2acf32b9c68c3e2147f7fa517232
Yuvraj Singh <98007961+SinghYuvraj0506@users.noreply.github.com>
2022-10-27 02:37:10 +0530

    updated db

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
-const mongouri = "mongodb+srv://SinghYuvraj0506:Yohoney%402002@cluster0.nuuez0u.mongodb.net/amazon_Clone?retryWrites=true&w=majority"
-//const mongouri = "mongodb://localhost:27017/amazon_Clone"
-
 
 const connectToMongo = () =>{
-    mongoose.connect(mongouri,()=>{
+    mongoose.connect(process.env.MONGOURI,()=>{
         console.log("Connectd to Mongo Succesfully")
     })
 } 
